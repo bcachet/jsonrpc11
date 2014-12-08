3acb8a017f7f8f2cccc18f517845f012625a6fe5
Bertrand Cachet <bertrand.cachet@gmail.com>
2014-12-08 23:33:53 +0100

    ADD: Proove that we can compare Json::shape

--- tests/json11_test.cpp
+++ tests/json11_test.cpp
@@ -1,25 +1,9 @@
 #include <string>
 #include <catch.hpp>
 #include <json11.hpp>
-
+#include <list>
 using namespace json11;
 using std::string;
-
-TEST_CASE("Json object initialisation", "[json11]" ) {
-  Json circle = Json::object {{"center", Json::object {{"x", 10}, {"y", 10}}},
-                              {"radius", 10}};
-  const string circle_as_str = R"({"center": {"x": 10, "y": 10}, "radius": 10})";
-  REQUIRE( circle.dump() == circle_as_str);
-}
-
-TEST_CASE("Json object as params definition", "[json11]") {
-  Json::shape def = {{"center", Json::OBJECT},
-                     {"radius", Json::NUMBER}};
-  const string params_as_str = R"({"center": {"x": 10, "y": 10}, "radius": 10})";
-  string err = "";
-  Json obj = Json::parse(params_as_str, err);
-  REQUIRE(obj.has_shape(def, err));
-}
 
 class Point {
 public:
@@ -29,11 +13,48 @@ public:
   Point (Json o) : x(o["x"].int_value()), y(o["y"].int_value()) {}
 };
 
-TEST_CASE("Json type conversion", "[json11]") {
-  const string point_as_str = R"({"x": 10, "y": 20})";
+const Json circle = Json::object {{"center", Json::object {{"x", 10}, {"y", 20}}},
+                                  {"radius", 10}};
+const string circle_as_str = R"({"center": {"x": 10, "y": 20}, "radius": 10})";
+const Json::shape circle_def =  {{"center", Json::OBJECT},
+                          {"radius", Json::NUMBER}};
+
+TEST_CASE("Json object initialisation", "[json11]" ) {
+  REQUIRE(circle.dump() == circle_as_str);
   string err = "";
-  Json point_as_json = Json::parse(point_as_str, err);
-  Point p = Point(point_as_json);
+  REQUIRE(Json::parse(circle_as_str, err) == circle);
+}
+
+TEST_CASE("Json object as params definition", "[json11]") {
+  string err = "";
+  REQUIRE(circle.has_shape(circle_def, err));
+}
+
+TEST_CASE("Json type conversion", "[json11]") {
+  string err = "";
+  Point p = Point(Json::parse(circle["center"].dump(), err));
   REQUIRE(p.x == 10);
   REQUIRE(p.y == 20);
+}
+
+bool compare(Json::shape const&  l, Json::shape const& r) {
+  auto sort_params = [](std::pair<std::string, Json::Type> l,
+      std::pair<std::string, Json::Type> r) {
+    return l.first.compare(r.first) > 0;
+  };
+  if (l.size() == r.size()) {
+    std::list<std::pair<std::string, Json::Type>> ll(l), rl(r);
+    ll.sort(sort_params); rl.sort(sort_params);
+    return std::equal(ll.begin(), ll.end(), rl.begin());
+  }
+  return false;
+}
+
+TEST_CASE("Json shape can be compared") {
+  Json::shape same_def = {{"center", Json::OBJECT}, {"radius", Json::NUMBER}};
+  Json::shape same_def_but_mixed = {{"radius", Json::NUMBER}, {"center", Json::OBJECT}};
+  Json::shape different_def = {{"center", Json::ARRAY}, {"radius", Json::NUMBER}};
+  REQUIRE(compare(same_def, circle_def));
+  REQUIRE(compare(same_def_but_mixed, circle_def));
+  REQUIRE_FALSE(compare(different_def, circle_def));
 }
