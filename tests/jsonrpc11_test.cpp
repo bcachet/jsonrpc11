66156c1743f940850756b5eb7a4846c6f6c66881
Bertrand Cachet <bertrand.cachet@gmail.com>
2014-12-18 13:54:33 +0100

    REF: Split tests in adapted sections

--- tests/jsonrpc11_test.cpp
+++ tests/jsonrpc11_test.cpp
@@ -10,81 +10,90 @@ using namespace jsonrpc11;
 using std::string;
 using std::initializer_list;
 
-Json add(std::list<double>const& values) {
-  double result = std::accumulate(values.cbegin(), values.cend(), 0.0, [](double &res, double const& x)
-  {
-    return res += static_cast<double>(x);
-  });
-  return Json::object{ { "result", result } };
-}
 
-
-Json say(std::string what, int times )
+TEST_CASE("Check Json-Rpc message validity", "[jsonrpc]")
 {
-  for (int i = 0; i < times; ++i) {
-    std::cout << what << std::endl;
+  JsonRpcHandler server;
+  SECTION("Invalid message")
+  {
+    auto result = server.handle(R"({"method": "add", "params": {"a": 1, "b": 1}, "id": 1})");
+    REQUIRE(result.get_status() == JsonRpcResponse::INVALID_MSG);
   }
-  return Json();
+
+  SECTION("Invalid method name")
+  {
+    auto result = server.handle(R"({"jsonrpc": "2.0", "method": "aaddd", "params": {"a": 1, "b": 1}, "id": 1})");
+    REQUIRE(result.get_status() == JsonRpcResponse::METHOD_NOT_FOUND);
+  }
 }
 
-TEST_CASE("Extract method name and parameters from Json-Rpc", "[jsonrpc]") {
-  Json::shape echo = {{"jsonrpc", Json::STRING}, {"method", Json::STRING}, {"params", Json::ARRAY}};
-  SECTION("Json-Rpc message with method name and params") {
-    initializer_list<string> valid_msgs = {
-        R"({"jsonrpc": "2.0", "method": "echo", "params": ["Hello JSON-RPC"], "id": 1})",
-        R"({"jsonrpc": "2.0", "method": "echo", "params": ["Hello", "JSON-RPC"], "id": 1})",
-        R"({"jsonrpc": "2.0", "method": "echo", "params": [], "id": 1})",
-    };
-    string err;
-    std::for_each(valid_msgs.begin(), valid_msgs.end(), [&echo, &err](string msg) {
-      auto json_rpc = Json::parse(msg, err);
-      REQUIRE(json_rpc.has_shape(echo, err));
-    });
+
+TEST_CASE("Named params with simple types support", "[jsonrpc]")
+{
+  JsonRpcHandler server;
+  std::function<Json(std::string, int)> say = [](std::string what, int times)
+  {
+    REQUIRE(what == "fu");
+    REQUIRE(times == 3);
+    return Json();
+  };
+  server.register_function("say", { { "what", Json::STRING }, { "times", Json::NUMBER } }, say);
+
+  SECTION("Params with invalid type")
+  {
+    auto response = server.handle(R"({"jsonrpc": "2.0", "method": "say", "params": {"what": "fu", "times": "3"}, "id": 1})");
+    REQUIRE(response.get_status() == JsonRpcResponse::INVALID_PARAMS);
   }
 
-  SECTION("Json-Rpc message with method name and params") {
-    initializer_list<string> invalid_msgs = {
-        R"({"jsonrpc": "2.0", "method": "echo", "id": 1})",
-        R"({"jsonrpc": "2.0", "method": "echo", "params": "{"param1" : null}", "id": 1})",
-        R"({"method": "echo", "params": ["Hello JSON-RPC"], "id": 1})",
-    };
-    string err;
-    std::for_each(invalid_msgs.begin(), invalid_msgs.end(), [&echo, &err](string msg) {
-      auto json_rpc = Json::parse(msg, err);
-      REQUIRE_FALSE(json_rpc.has_shape(echo, err));
-    });
+  SECTION("Params with invalid name")
+  {
+    auto response = server.handle(R"({"jsonrpc": "2.0", "method": "say", "params": {"what": "fu", "unknown": 3}, "id": 1})");
+    REQUIRE(response.get_status() == JsonRpcResponse::INVALID_PARAMS);
   }
 
-  SECTION("Bind method to JsonRpc Server") {
+  SECTION("Valid params")
+  {
+    auto response = server.handle(R"({"jsonrpc": "2.0", "method": "say", "params": {"what": "fu", "times": 3}, "id": 1})");
+    REQUIRE(response.get_status() == JsonRpcResponse::OK);
+  }
+}
+
+TEST_CASE("Named params with complex types support", "[jsonrpc]") {
+  JsonRpcHandler server;
+  class Talker
+  {
+    std::string what;
+  };
+
+  std::function<Json(Json, int)> say = [](Json talker, int times) {
+    REQUIRE(talker["what"].string_value() == "fu");
+    REQUIRE(times == 3);
+    return Json();
+  };
+  server.register_function("say", { { "talker", Json::OBJECT }, { "times", Json::NUMBER } }, say);
+
+  SECTION("Params with complex type") {
+    auto response = server.handle(R"({"jsonrpc": "2.0", "method": "say", "params": {"talker": { "what": "fu" }, "times": 3}, "id": 1})");
+    REQUIRE(response.get_status() == JsonRpcResponse::OK);
+  }
+}
+
+
+TEST_CASE("Json-Rpc Handler", "[jsonrpc]") {
+  
+  SECTION("Positional params method") {
     JsonRpcHandler server;
-    std::function<Json(double, double)> add = [](double const& a, double const& b)->Json {return a + b;};
-    server.register_function("add", {{"a", Json::NUMBER}, {"b", Json::NUMBER}}, add);
-    JsonRpcResponse result = server.handle(R"({"jsonrpc": "2.0", "method": "add", "params": {"a": 1, "b": 1}, "id": 1})");
-    REQUIRE(result.get_status() == JsonRpcResponse::OK);
-    result = server.handle(R"({"jsonrpc": "2.0", "method": "aaddd", "params": {"a": 1, "b": 1}, "id": 1})");
-    REQUIRE(result.get_status() == JsonRpcResponse::METHOD_NOT_FOUND);
-    result = server.handle(R"({"method": "add", "params": {"a": 1, "b": 1}, "id": 1})");
-    REQUIRE(result.get_status() == JsonRpcResponse::INVALID_MSG);
-    result = server.handle(R"({"jsonrpc": "2.0", "method": "add", "params": {"a": 1, "unknown": 1}, "id": 1})");
-    REQUIRE(result.get_status() == JsonRpcResponse::INVALID_PARAMS);
-  }
-
-  SECTION("Add with array params") {
-    JsonRpcHandler server;
-    server.register_function<double>("add", { Json::NUMBER }, add);
+    server.register_function<double>("add", { Json::NUMBER }, [](std::list<double>const& values) {
+      double result = std::accumulate(values.cbegin(), values.cend(), 0.0, [](double &res, double const& x)
+      {
+        return res += static_cast<double>(x);
+      });
+      return Json::object{ { "result", result } };
+    });
     JsonRpcResponse result = server.handle(R"({"jsonrpc": "2.0", "method": "add", "params": [ 1, 1, 1], "id": 1})");
     REQUIRE(result.get_status() == JsonRpcResponse::OK);
     REQUIRE(result.get_response()["result"].int_value() == 3);
     result = server.handle(R"({"jsonrpc": "2.0", "method": "add", "params": {"a": 1, "b": 1}, "id": 1})");
     REQUIRE(result.get_status() == JsonRpcResponse::INVALID_PARAMS);
   }
-
-  SECTION("Create FunctionDefinition obj")
-  {
-    std::string err;
-    FunctionDefinition* fd = new FunctionDefitionWithPositionalParams<double>({ Json::NUMBER }, add);
-    Json result = fd->call_with_params(Json::parse(R"([1, 1, 1])", err));
-    REQUIRE((int)(result["result"].number_value()) == 3);
-  }
-
 }
